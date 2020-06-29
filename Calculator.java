c670f0245ae08adcb775304a243b44767352cfa5
ymdevx3 <y.mori.707@gmail.com>
2020-06-30 01:32:23 +0900

    正規表現による数式チェック処理を追加

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
+import java.util.regex.Pattern;
 import jp.hishidama.eval.*;
 
 public class Calculator {
@@ -6,7 +7,16 @@ public class Calculator {
      * 計算
      * @param expression 数式文字列
      */
-    public static double calculate(String expression) {
+    public static double calculate(String expression) throws IllegalArgumentException {
+
+        // 正規表現チェック（数値と演算子[+, -, *, /]のみ使用かの場合s）
+        expression = expression.replace(" ", "");
+        // ^[\+|-]?[\d|\+|\-|\*|\/]*[\d]+$
+        String pattern = "^[\\+|\\-]?[\\d|\\+|\\-|\\*|/]*[\\d]+$";
+        Pattern p = Pattern.compile(pattern);
+        if (!p.matcher(expression).find()) {
+            throw new IllegalArgumentException("式が不正です。");
+        }
 
         Rule rule = ExpRuleFactory.getDefaultRule();
         Expression exp = rule.parse(expression); //解析
