8a46c41dbef8c0c0d7cf74c9a67135c5ef37330f
ymdevx3 <y.mori.707@gmail.com>
2020-06-30 01:34:17 +0900

    Fix comment

--- Calculator.java
+++ Calculator.java
@@ -4,12 +4,13 @@ import jp.hishidama.eval.*;
 public class Calculator {
     
     /**
-     * 計算
+     * 文字列から計算結果を取得
      * @param expression 数式文字列
+     * @return 計算結果
      */
     public static double calculate(String expression) throws IllegalArgumentException {
 
-        // 正規表現チェック（数値と演算子[+, -, *, /]のみ使用かの場合s）
+        // 正規表現チェック（数値と演算子[+, -, *, /]のみ使用可の場合）
         expression = expression.replace(" ", "");
         // ^[\+|-]?[\d|\+|\-|\*|\/]*[\d]+$
         String pattern = "^[\\+|\\-]?[\\d|\\+|\\-|\\*|/]*[\\d]+$";
@@ -19,8 +20,8 @@ public class Calculator {
         }
 
         Rule rule = ExpRuleFactory.getDefaultRule();
-        Expression exp = rule.parse(expression); //解析
-        double result = exp.evalDouble(); //計算実施
+        Expression exp = rule.parse(expression);    //解析
+        double result = exp.evalDouble();           //計算実施
 
         return result;
     }
